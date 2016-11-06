160547d10b1d6f686edf60b2c34d02c69cd49227
Clive Chan <cc@clive.io>
2016-11-06 09:07:43 -0500

    add nfc links

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.use('/toast', require('./lib/photon'));
 app.post('/update', require('./lib/update'));
 app.use('/static', express.static('static'));
 
+let callPhoton = require('./lib/photon').callPhoton;
+app.get('/td', (req, res) => callPhoton('toast', '{"length": 1, "data": [5]}'));
+app.get('/ml', (req, res) => callPhoton('toast', '{"length": 2, "data": [1,4]}'));
+app.get('/ag', (req, res) => callPhoton('toast', '{"length": 2, "data": [0,5]}'));
+
 app.all('*', (req, res) => res.sendStatus(404))
 app.use((err, req, res, next) => res.sendStatus(500))
 
