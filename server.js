9a9023e36f8137867f3f9d0d1b02f8b4c5f0bcd3
Clive Chan <cc@clive.io>
2016-11-06 09:54:33 -0500

    update agri

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/static', express.static('static'));
 let callPhoton = require('./lib/photon').callPhoton;
 app.get('/td', (req, res) => callPhoton('toast', '{"length": 1, "data": [5]}'));
 app.get('/ml', (req, res) => callPhoton('toast', '{"length": 2, "data": [1,4]}'));
-app.get('/ag', (req, res) => callPhoton('toast', '{"length": 2, "data": [0,5]}'));
+app.get('/ag', (req, res) => callPhoton('toast', '{"length": 4, "data": [0,1,4,5]}'));
 
 app.all('*', (req, res) => res.sendStatus(404))
 app.use((err, req, res, next) => res.sendStatus(500))
