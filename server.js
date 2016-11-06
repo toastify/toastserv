659fa56effc230561cf762b8ea31ea0b47506f42
Clive Chan <cc@clive.io>
2016-11-05 20:46:54 -0400

    finally finished refactor cs => js

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+require('./audioserv');
+
+const PORT = process.env.PORT || 1337;
+
+let express = require('express');
+let helmet = require('helmet');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(helmet());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => res.sendFile('index.html', { root: './static' }));
+app.use('/toast', require('./lib/photon'));
+app.post('/update', require('./lib/update'));
+app.use('/static', express.static('static'));
+
+app.all('*', (req, res) => res.sendStatus(404))
+app.use((err, req, res, next) => res.sendStatus(500))
+
+app.listen(PORT, 'localhost', => console.log("listening on * : " + PORT));
